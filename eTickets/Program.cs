566c2deddcffed13fdf2952338659655862eb175
karthickkrish19 <karthickkain@gmail.com>
2023-01-24 17:47:29 +0530

    Changes

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,37 @@
+using eTickets.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Start Changes
+
+//DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings")));
+
+////Services configuration
+//builder.Services.AddScoped<IActorsService, ActorsService>();
+//builder.Services.AddScoped<IProducersService, ProducersService>();
+//builder.Services.AddScoped<ICinemasService, CinemasService>();
+//builder.Services.AddScoped<IMoviesService, MoviesService>();
+//builder.Services.AddScoped<IOrdersService, OrdersService>();
+
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+////Authentication and authorization
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+//builder.Services.AddMemoryCache();
+//builder.Services.AddSession();
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//});
+
+//End Changes
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +50,17 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+//app.UseSession();
 
 app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed database
+AppDbInitializer.Seed(app);
+//AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
 
 app.Run();
